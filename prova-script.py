3421eb9827353b807bcbaf275f02045319b7c0e9
albruz <andrea.albrigoni@gmail.com>
2016-06-07 15:50:02 +0200

    correzione errore

--- prova-script.py
+++ prova-script.py
@@ -2,64 +2,64 @@ import time
 
 def executeSomething():
 
-consumer_key        = "ABCDEFGHIJKLKMNOPQRSTUVWXYZ"
-consumer_secret     = "1234567890ABCDEFGHIJKLMNOPQRSTUVXYZ"
-access_token        = "ZYXWVUTSRQPONMLKJIHFEDCBA"
-access_token_secret = "0987654321ZYXWVUTSRQPONMLKJIHFEDCB"
+	consumer_key        = "ABCDEFGHIJKLKMNOPQRSTUVWXYZ"
+	consumer_secret     = "1234567890ABCDEFGHIJKLMNOPQRSTUVXYZ"
+	access_token        = "ZYXWVUTSRQPONMLKJIHFEDCBA"
+	access_token_secret = "0987654321ZYXWVUTSRQPONMLKJIHFEDCB"
 
-messages = [
-    "come state?",
-    "io molto bene!",
-    "oggi fa caldo!",
-    "domani piove!",
-    "che bel sole!",
-    "speriamo faccia bello..",
-    "che bella giornata!",
-    "che sonno!",
-    "si lavora!",
-    "pyhton <3",
-    "polpette!",
-    "ikea!",
-    "computer <3",
-    "fame!",
-    "sete!",
-    "biscotti <3",
-    "torta!",
-    "patatine <3",
-    "stanchissimo..",
-    "manca poco ;)",
-    "super caldo!",
-    "piscina!",
-    "argentina!",
-    "calcio!",
-    "forza ragazzi!",
-]
-
-
-import random
-
-from twython import Twython
-
-from auth import (
-    consumer_key,
-    consumer_secret,
-    access_token,
-    access_token_secret
-)
-
-twitter = Twython(
-    consumer_key,
-    consumer_secret,
-    access_token,
-    access_token_secret
-)
+	messages = [
+    		"come state?",
+    		"io molto bene!",
+    		"oggi fa caldo!",
+    		"domani piove!",
+    		"che bel sole!",
+    		"speriamo faccia bello..",
+    		"che bella giornata!",
+    		"che sonno!",
+    		"si lavora!",
+    		"pyhton <3",
+    		"polpette!",
+    		"ikea!",
+    		"computer <3",
+    		"fame!",
+    		"sete!",
+    		"biscotti <3",
+    		"torta!",
+    		"patatine <3",
+    		"stanchissimo..",
+    		"manca poco ;)",
+    		"super caldo!",
+    		"piscina!",
+    		"argentina!",
+    		"calcio!",
+    		"forza ragazzi!",
+	]
 
 
-message = random.choice(messages)
-twitter.update_status(status=message)
-print("Tweeted: %s" % message)
+	import random
 
-time.sleep(5)
+	from twython import Twython
 
-while True:
-    executeSomething()
+	from auth import (
+    		consumer_key,
+    		consumer_secret,
+    		access_token,
+    		access_token_secret
+	)
+
+	twitter = Twython(
+    		consumer_key,
+    		consumer_secret,
+    		access_token,
+    		access_token_secret
+	)
+
+
+	message = random.choice(messages)
+	twitter.update_status(status=message)
+	print("Tweeted: %s" % message)
+
+	time.sleep(5)
+
+	while True:
+    		executeSomething()
